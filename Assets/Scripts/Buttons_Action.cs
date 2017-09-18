461e2743f8855f8962f05ff395069f7ce9145c59
Rafael Freire <rafael_escultor@hotmail.com>
2017-09-18 10:31:21 +0100

    Main Screen Creation
    -First Screen has been created
    -Basic Animation added
    -Stack of buttons and Scene load.
    -Exit Button added

---
+++ Assets/Scripts/Buttons_Action.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+
+public class Buttons_Action : MonoBehaviour{
+
+
+	public void OnQuitButtonClick(){
+
+		Application.Quit ();
+
+	}
+	public void OnButtonClick(){
+
+		SceneManager.LoadScene(gameObject.tag, LoadSceneMode.Additive);
+
+	}
+
+}
